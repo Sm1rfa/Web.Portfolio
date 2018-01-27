586570aa42ce63c735c4eb0b892b952ca9eb8d61
Stoyan Bonchev <sm1rfs88@gmail.com>
2018-01-27 16:04:29 +0100

    add changes

--- README.md
+++
@@ -1,2 +0,0 @@
-# Web.Portfolio
-My personal web page made in AngularJS 4.
--- stoyan.portfolio/src/app/app.component.ts
+++ stoyan.portfolio/src/app/app.component.ts
@@ -9,9 +9,11 @@ export class AppComponent {
   title = 'Stoyan Bonchev';
   linkedInIcon: string;
   githubIcon: string;
+  certBadgePath: string;
 
   constructor() {
     this.linkedInIcon = '/assets/images/linkedin.png';
     this.githubIcon = '/assets/images/github.png';
+    this.certBadgePath = '/assets/images/psd1-large.png';
   }
 }
